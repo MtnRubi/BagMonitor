e42e48706ad56b367da5bb14e6ea55992d709e6b
Don Krause <dkrause@unixplanet.net>
2017-09-20 19:35:04 -0700

    changed to official thingspeak site, new api key

--- postTS.py
+++ postTS.py
@@ -5,9 +5,9 @@ import httplib
 import urllib
 
 def doit():
-    params = urllib.urlencode({'field1': sys.argv[1], 'field2': sys.argv[2], 'field3': sys.argv[3], 'field4': sys.argv[4],'field5': sys.argv[5], 'key':'CY830JE8XP8B2WM4'})
+    params = urllib.urlencode({'field1': sys.argv[1], 'field2': sys.argv[2], 'field3': sys.argv[3], 'field4': sys.argv[4],'field5': sys.argv[5], 'key':'1VM2S9Z7RJRH8M6A'})
     headers = {"Content-type": "application/x-www-form-urlencoded","Accept":"text/plain"}
-    conn = httplib.HTTPConnection("scudder.optivus.com:3000")
+    conn = httplib.HTTPConnection("api.thingspeak.com")
     conn.request("POST","/update",params,headers)
     response = conn.getresponse()
     print response.status, response.reason
