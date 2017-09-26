0e097df6c79b93dc3723e75a6be9ebf2b8182974
Don Krause <dkrause@unixplanet.net>
2017-09-25 18:34:23 -0700

    Crappy reboot script for crappy USB on the pi

---
+++ monitorBoard.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+#
+# Can't believe I have to do this. This script runs as root under supervisor
+# which makes it too easy since it doesn't have to fork.
+#
+# If it sees ttyACM1 in the log files, that means the stupid Raspberry droped the connect to the arduino... again
+# So it reboots the pi hard.
+use strict;
+
+use File::Tail;
+use Unix::Syslog qw(:subs);
+use Unix::Syslog qw(:macros);
+
+syslog(LOG_NOTICE,"Starting cheesy board monitor");
+
+my $file=File::Tail->new(name=>"/var/log/kern.log", maxinterval=>300);
+
+while(defined(my $line=$file->read)) {
+	print "$line";
+	if ($line =~ /ttyACM1/) {
+		print("Rebooting because ttyACM1\n");
+		syslog(LOG_NOTICE,"Rebooting because ttyACM1");
+		`reboot`;
+	}
+}
